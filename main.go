c4ada985902d053a0f86088bcd39b4aae8b70fb9
Laxman B <dev.laxmanb@gmail.com>
2025-06-13 20:27:31 +0530

    setup server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
 
+	// create router
+	r := gin.Default()
+
+	// two handlers
+	// post url to shortenize the url
+	// get url to fetch the url
+
+	r.POST("/")
+	r.GET("/")
+
+	r.Run(":8123")
 }
